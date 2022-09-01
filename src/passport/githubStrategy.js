7ceeac49b2d485988cc581a6f494889a7695e5e8
oddythecreative <oddythecreative@gmail.com>
2022-09-02 01:58:50 +0900

    feat: passport 수정

--- src/passport/githubStrategy.js
+++ src/passport/githubStrategy.js
@@ -1,13 +1,13 @@
-import passport from 'passport';
-import dotenv from 'dotenv';
+import passport from "passport";
+import dotenv from "dotenv";
 
-dotenv.config({ path: '../.env' });
+dotenv.config({ path: "../.env" });
 
 // const GitHubStrategy = require('passport-github').Strategy;
 
-import GitHubStrategy from 'passport-github';
+import GitHubStrategy from "passport-github";
 
-import User from '../models/user.js';
+import User from "../models/user.js";
 
 let id = process.env.GIT_ID;
 let secret = process.env.GIT_SECRET;
@@ -18,10 +18,10 @@ const github = () => {
       {
         clientID: `${id}`,
         clientSecret: `${secret}`,
-        callbackURL: 'http://localhost:3000/api/auth/github/callback',
+        callbackURL: "http://13.124.114.140/api/auth/github/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
-        console.log('git profile', profile);
+        console.log("git profile", profile);
         try {
           const exUser = await User.findOne({
             where: { email: profile.profileUrl },
--- src/routes/auth_route.js
+++ src/routes/auth_route.js
@@ -1,26 +1,26 @@
-import express from 'express';
-import passport from 'passport';
-import { isLoggedIn, isNotLoggedIn } from '../middlewares/authMiddleware.js';
+import express from "express";
+import passport from "passport";
+import { isLoggedIn, isNotLoggedIn } from "../middlewares/authMiddleware.js";
 // import AuthController from '../controllers/auth_controller.js';
 
 const router = express.Router();
 
-router.get('/github', passport.authenticate('github'));
+router.get("/github", passport.authenticate("github"));
 
 router.get(
-  '/github/callback',
-  passport.authenticate('github', {
-    failureRedirect: '/',
+  "/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: "/",
   }),
   (req, res) => {
-    res.json({ message: req.user });
+    res.redirect(`http://localhost:3000`);
   }
 );
 
-router.get('/logout', isLoggedIn, (req, res) => {
+router.get("/logout", isLoggedIn, (req, res) => {
   req.logout();
   req.session.destroy();
-  res.json({ message: 'logout' });
+  res.json({ message: "logout" });
 });
 
 export default router;
