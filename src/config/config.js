24f963fdc32ed52ae0477bf5f802e37c58a45f00
taesikyoon <90764424+taesikyoon@users.noreply.github.com>
2022-09-06 17:17:34 +0900

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,6 @@ const config = {
     password: process.env.DB_PW,
     database: process.env.DB,
     host: process.env.DB_HOST,
-    host: '127.0.0.1',
     dialect: 'mysql',
     logging: false,
     timezone: '+09:00',
