77f4c501ae4bcfce26d75e36fe09a5e42244fe21
Taesik yoon <taesikyoon97@naver.com>
2022-09-10 18:59:42 +0900

    test test with front

--- src/services/search_service.js
+++ src/services/search_service.js
@@ -1,6 +1,20 @@
+import Qna from '../models/qna.js';
+import sequelize from 'sequelize';
+import QnaTag from '../models/qna_tag.js';
+const Op = sequelize.Op;
 class SearchService {
-  Search = () => {
-    console.log('gd');
+  Search = async (data) => {
+    const newdata = await Qna.findAll({
+      where: {
+        [Op.or]: [
+          { title: { [Op.like]: '%' + data + '%' } },
+          { content: { [Op.like]: '%' + data + '%' } },
+          ,
+        ],
+      },
+    });
+    console.log(newdata.length);
+    return newdata;
   };
 }
 
