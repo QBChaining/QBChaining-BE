9a63f13a4a75f0f84185afe6848f0246d8ad0294
kpzzy <sdjjm95@naver.com>
2022-09-19 21:30:35 +0900

    FEAT make repository folder

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,22 +1,22 @@
 import express from 'express';
 import PostController from '../controllers/post_controller.js';
 import verifyToken from '../middlewares/auth.js';
-import check_signin from '../middlewares/check_signin.js';
+import checkSignin from '../middlewares/check.signin.js';
 import SearchCotroller from '../controllers/search_controller.js';
 const router = express.Router();
 
 const postController = new PostController();
 const searchCotroller = new SearchCotroller();
 
-router.get('/', check_signin, postController.PostShowAll);
-router.get('/search', check_signin, searchCotroller.PostSearch);
-router.get('/comment', check_signin, postController.PostShowComment);
-router.get('/like', check_signin, postController.PostShowLike);
-router.get('/hits', check_signin, postController.PostShowhit);
+router.get('/', checkSignin, postController.PostShowAll);
+router.get('/search', checkSignin, searchCotroller.PostSearch);
+router.get('/comment', checkSignin, postController.PostShowComment);
+router.get('/like', checkSignin, postController.PostShowLike);
+router.get('/hits', checkSignin, postController.PostShowhit);
 router.get('/bookmark', verifyToken, postController.PostBookMarkView);
-router.get('/tags', check_signin, postController.PostTagShow);
-router.get('/:post_id', check_signin, postController.PostShowOne);
-router.get('/users/:user_name', check_signin, postController.PostShowMy);
+router.get('/tags', checkSignin, postController.PostTagShow);
+router.get('/:post_id', checkSignin, postController.PostShowOne);
+router.get('/users/:user_name', checkSignin, postController.PostShowMy);
 
 router.post('/', verifyToken, postController.PostCreate);
 router.put('/:post_id', verifyToken, postController.PostUpdate);
