085ddf169e84254168b64542c6af89180290c774
jihun <oddythecreative@gmail.com>
2022-09-19 21:38:32 +0900

    user info modification

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import AuthService from '../services/authService.js';
+import AuthService from '../services/auth.service.js';
 
 export default class AuthController {
   authService = new AuthService();
