a62db1acd051a3d2fb5578e784fcd507041465ac
kpzzy <sdjjm95@naver.com>
2022-09-26 16:20:58 +0900

    Fix comment pagenation

--- src/services/post.comment.service.js
+++ src/services/post.comment.service.js
@@ -12,8 +12,12 @@ import PostCommentRepository from '../repositories/post.comment.repository.js';
 export default class PostCommentServices {
   postCommentRepository = new PostCommentRepository();
 
-  CommentShowAll = async (postId) => {
-    const postcomment = await this.postCommentRepository.CommentShowAll(postId);
+  CommentShowAll = async (postId, page, page_count) => {
+    const postcomment = await this.postCommentRepository.CommentShowAll(
+      postId,
+      page,
+      page_count
+    );
     if (!postcomment) throw new BadRequestException('게시물이 없습니다');
     if (postcomment)
       return postcomment.map((comment) => {
@@ -42,8 +46,6 @@ export default class PostCommentServices {
       userName,
       postId
     );
-
-    // 여기쯤부터 하면댈듯
     const findBookMark = await this.postCommentRepository.PostBookmark(postId);
     if (findBookMark.length === 0) {
       const notification = await this.postCommentRepository.Notification(
